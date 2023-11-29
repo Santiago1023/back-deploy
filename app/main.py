a19b093f84fe6d9690d592897b26226adf97260a
andresqb198 <andresqb198@gmail.com>
2023-11-29 17:45:00 -0500

    :sparkles: [ADD] Prometheus and Grafana feature

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes import auth, sign, user, models, streaming
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
@@ -18,4 +19,6 @@ app.include_router(user.router)
 app.include_router(models.router)
 app.include_router(streaming.router)
 
+# Instrumenta tu aplicación para exponer métricas Prometheus en /metrics
+Instrumentator().instrument(app).expose(app)
 ##! [] crate token persistance change on login
